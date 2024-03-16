bc73ff0600581afd98b5c77ced7ac8b558675a6e
April Gips <msg558@gmail.com>
2024-03-16 17:13:12 -0500

    adds video:

--- server.py
+++ server.py
@@ -6,7 +6,17 @@ app = FastAPI()
 
 # Serve files from the "assets" folder
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+app.mount("/video", StaticFiles(directory="./video"), name="video")
+
 
 @app.get("/")
 async def main():
     return FileResponse('index.html')
+
+@app.get("/open")
+async def main():
+    return 'open response'
+
+@app.get("/close")
+async def main():
+    return 'close response'
